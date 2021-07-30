496b171e8f90e5a5c5c5b1258fdbc26f9df07100
joenapper <joe.napper@hotmail.co.uk>
2021-07-30 16:49:51 +0100

    Linked vector icons and created Home screen

---
+++ screens/Home/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/Fontisto';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Pressable style={styles.searchButton}>
+        <Icon name="search" size={25} color={'#f15454'} />
+        <Text style={styles.searchButtonText}>Where are you going?</Text>
+      </Pressable>
+
+      <ImageBackground
+        source={require('../../assets/images/wallpaper.jpg')}
+        style={styles.image}>
+        <Text style={styles.title}>Go Near</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 500,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: '#fff',
+    width: '70%',
+    marginLeft: 25,
+  },
+  button: {
+    backgroundColor: '#fff',
+    width: 200,
+    height: 40,
+    borderRadius: 10,
+    marginTop: 25,
+    marginLeft: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  searchButton: {
+    backgroundColor: '#fff',
+    height: 60,
+    width: Dimensions.get('screen').width - 20,
+    borderRadius: 30,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 20,
+    zIndex: 100,
+  },
+  searchButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default HomeScreen;
