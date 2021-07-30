2f530d1eca085d6fec6dc80eeef8dc54dfc857e6
joenapper <joe.napper@hotmail.co.uk>
2021-07-30 17:28:42 +0100

    Created Post component with placeholder content

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
-import HomeScreen from './screens/Home';
+// import HomeScreen from './screens/Home';
+import Post from './components/Post';
 
 const App: () => Node = () => {
   return (
     <SafeAreaView>
       <StatusBar />
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <Post />
     </SafeAreaView>
   );
 };
