0db07ec8bffaaee7763a81f37c5c9934e056c9c1
joenapper <joe.napper@hotmail.co.uk>
2021-08-01 17:17:43 +0100

    Created search results screen and listing dummy data posts

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 // import HomeScreen from './screens/Home';
+import SearchResultsScreen from './screens/SearchResults';
 import Post from './components/Post';
+import feed from './assets/data/feed';
+
+const post1 = feed[0];
 
 const App: () => Node = () => {
   return (
     <SafeAreaView>
       <StatusBar />
       {/* <HomeScreen /> */}
-      <Post />
+      {/* <Post post={post1} /> */}
+      <SearchResultsScreen />
     </SafeAreaView>
   );
 };
--- components/Post.js
+++ components/Post.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
 
-const Post = () => {
+const Post = ({post}) => {
   return (
     <View style={styles.container}>
       <Image
         style={styles.image}
         source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: post.image,
         }}
       />
 
-      <Text style={styles.bedrooms}>1 bed 1 bathroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed(s) {post.bedroom} bedroom(s)
+      </Text>
 
       <Text style={styles.description} numberOfLines={2}>
-        Entire Flat. Puerto de la cruz lrmLorem Ipsum is simply dummy text of
-        the printing and typesetting industry. Lorem Ipsum has been the
-        industry's standard dummy.
+        {post.type}. {post.title}
       </Text>
 
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>£36</Text>
-        <Text style={styles.price}> £30 </Text>
+        <Text style={styles.oldPrice}>£{post.oldPrice}</Text>
+        <Text style={styles.price}> £{post.newPrice} </Text>/ night
       </Text>
 
-      <Text style={styles.totalPrice}>£230 total</Text>
+      <Text style={styles.totalPrice}>£{post.totalPrice} total</Text>
     </View>
   );
 };
