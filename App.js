76f3e1a9d155a7417b9159a264aa6af75da0e963
joenapper <joe.napper@hotmail.co.uk>
2021-08-03 14:13:30 +0100

    Added and tested navigation dependencies

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
-// import HomeScreen from './screens/Home';
-// import SearchResultsScreen from './screens/SearchResults';
-// import DestinationSearchScreen from './screens/DestinationSearch';
-import GuestsScreen from './screens/Guests';
-// import Post from './components/Post';
-import feed from './assets/data/feed';
-
-const post1 = feed[0];
+import Router from './navigation/Router';
 
 const App: () => Node = () => {
   return (
-    <SafeAreaView>
+    <>
       <StatusBar />
-      {/* <HomeScreen /> */}
-      {/* <Post post={post1} /> */}
-      {/* <SearchResultsScreen /> */}
-      {/* <DestinationSearchScreen /> */}
-      <GuestsScreen />
-    </SafeAreaView>
+      <Router />
+    </>
   );
 };
 
